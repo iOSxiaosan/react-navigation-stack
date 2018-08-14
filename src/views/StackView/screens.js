bfa1d5516537c445ec8dc4fddc3ea8a491043681
Krzysztof Magiera <krzys@swmansion.com>
2018-08-14 14:47:48 +0200

    Use react-native-screens when native screen components are available

--- src/views/StackView/screens.js
+++ src/views/StackView/screens.js
@@ -1,6 +1,46 @@
-import { Animated, View } from 'react-native';
+import React from 'react';
+import { Animated, View, UIManager, StyleSheet } from 'react-native';
 
-const ScreenContainer = View;
-const Screen = Animated.View;
+let ScreenContainer = View;
+let Screen = Animated.View;
+
+// If RNSScreen native component is available, instead of using plain RN Views
+// for ScreenContainer and Screen components, we can use native component
+// provided by react-native-screens lib. Note that we don't specify
+// react-native-screens as a dependency, but instead we check whether the library
+// is linked natively (we only `require` the lib if native components are installed)
+if (UIManager['RNSScreen']) {
+  // native screen components are available
+  const screens = require('react-native-screens');
+
+  const NativeScreen = screens.Screen;
+  class WrappedNativeScreen extends React.Component {
+    setNativeProps(props) {
+      this._ref.setNativeProps(props);
+    }
+    render() {
+      const { style, children, ...rest } = this.props;
+      return (
+        <NativeScreen
+          {...rest}
+          ref={ref => (this._ref = ref)}
+          style={StyleSheet.absoluteFill}
+        >
+          {/*
+            We need to wrap children in additional Animated.View because
+            of a bug in native driver preventing from both `active` and `styles`
+            props begin animated in `NativeScreen` component. Once
+            react-native/pull/20658 is merged we can export screens.Screen directly
+            and avoid wrapping with `Animated.View`.
+          */}
+          <Animated.View style={style}>{children}</Animated.View>
+        </NativeScreen>
+      );
+    }
+  }
+
+  Screen = WrappedNativeScreen;
+  ScreenContainer = screens.ScreenContainer;
+}
 
 export { ScreenContainer, Screen };
